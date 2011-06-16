11b24b38915e5d919ddb1f4168c107d6fd8b9376
Gera Janos <gerajanos@gmail.com>
2011-06-16 19:10:40 +0200

    jump to last unreaded message

--- js/content.js
+++ js/content.js
@@ -5,6 +5,48 @@ function removeChatWindow() {
 	$('table:eq(3) td:eq(2) br').remove();
 }
 
+var  jumpLastUnreadedMessage = {
+	
+	init : function() {
+		$('.b-h-o-head:eq(2)').next().find('a').each(function() {
+			
+			// If theres a new message
+			if($(this).find('small').length > 0) {
+			
+				// Get the new messages count
+				var newMsg = parseInt($(this).find('small').html().match(/\d+/g));
+				
+				// Get last msn's page number
+				var page = Math.ceil( newMsg / 80 );
+				
+				// Rewrite the url
+				$(this).attr('href', $(this).attr('href') + '&order=reverse&index='+page+'&newmsg='+newMsg+'');
+			}
+		});
+	}, 
+	
+	jump : function() {
+		
+		// Get new messages counter
+		var newMsg = document.location.href.split('&newmsg=')[1];
+
+		// Get the last msg
+		var lastMsg = newMsg % 80;
+		
+		// Target comment ele
+		var target = $('#ujhszjott').next().children('center:eq('+(lastMsg-1)+')');
+		
+		// Insert the horizontal rule
+		$('hr').insertBefore(target);
+		
+		// Target scrollTop
+		var targetOffset = $(target).offset().top;
+
+		$('body').animate({ scrollTop : targetOffset}, 1000);
+	}
+	
+};
+
 $(document).ready(function() {
 	
 	// FORUM.PHP
@@ -13,6 +55,20 @@ $(document).ready(function() {
 		// Remove chat window
 		if(dataStore['chat_hide'] == 'true') {
 			removeChatWindow();
+		}
+		
+		// Jump the last unreaded message
+		if(dataStore['jump_unreaded_messages'] == 'true') {
+			jumpLastUnreadedMessage.init();
+		}
+	}
+	
+	// LISTAZAS.PHP
+	else if(window.location.href.match('listazas.php3')) {
+	
+		// Jump the last unreaded message
+		if(dataStore['jump_unreaded_messages']) {
+			jumpLastUnreadedMessage.jump();
 		}
 	}
 });
