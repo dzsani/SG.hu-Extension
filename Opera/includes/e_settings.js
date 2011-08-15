bce3ea1381f8ba5a3791bc82dafa7527d93bda41
Gera Janos <gerajanos@gmail.com>
2011-08-15 21:11:07 +0200

    updated version number

--- Opera/includes/e_settings.js
+++ Opera/includes/e_settings.js
@@ -45,8 +45,8 @@ var cp = {
 			
 			html += '<div class="settings_page">';
 				html += '<h3>SG Fórum+</h3>';
-				html += '<p>Verzió: 0.5.1<br></p>';
-				html += '<p>Kiadás dátuma: 2011. 08. 14.</p>';
+				html += '<p>Verzió: 0.6.0<br></p>';
+				html += '<p>Kiadás dátuma: 2011. 08. 15.</p>';
 				html += '<p>Fejlesztő: Gera János "dzsani" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 				html += '<p>Közreműködők: Viszt Péter "passatgt" <a href="http://visztpeter.me" target="_blank">http://visztpeter.me</a>, Krupa György "pyro" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 			html += '</div>';
