59b604353beb3b70bf7da845fb244990bbfe7f13
Gera Janos <gerajanos@gmail.com>
2011-10-01 13:27:21 +0200

    updated version number

--- Firefox/data/js/settings.js
+++ Firefox/data/js/settings.js
@@ -37,7 +37,7 @@ var cp = {
 			
 			html += '<div class="settings_page">';
 				html += '<h3>SG Fórum+</h3>';
-				html += '<p>Verzió: 1.5.0<br></p>';
+				html += '<p>Verzió: 1.5.1<br></p>';
 				html += '<p>Kiadás dátuma: 2011. 10. 01.</p>';
 				html += '<p>Fejlesztő: Gera János "dzsani" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 				html += '<p>Közreműködők: Viszt Péter "passatgt" <a href="http://visztpeter.me" target="_blank">http://visztpeter.me</a>, Krupa György "pyro" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
