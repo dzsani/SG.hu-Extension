6e848c39c10f215a3b9093415995d13bd552206d
Gera Janos <gerajanos@gmail.com>
2012-01-24 22:10:42 +0100

    updated version number

--- Chrome/js/settings.js
+++ Chrome/js/settings.js
@@ -35,7 +35,7 @@ var cp = {
 			
 			html += '<div class="settings_page">';
 				html += '<h3>SG Fórum+</h3>';
-				html += '<p>Verzió: 2.3.0<br></p>';
+				html += '<p>Verzió: 2.4.0<br></p>';
 				html += '<p>Kiadás dátuma: 2012. 01. 24.</p>';
 				html += '<p>Fejlesztő: Gera János "dzsani" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 				html += '<p>Közreműködők: Viszt Péter "passatgt" <a href="http://visztpeter.me" target="_blank">http://visztpeter.me</a>, Krupa György "pyro" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
